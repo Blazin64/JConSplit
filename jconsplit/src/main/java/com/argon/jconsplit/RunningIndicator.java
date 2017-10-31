4a9c7e816aed9b5e788a9d7494bbfb0ce49d89fc
Thomas Goodman <5223071-Blazin64@users.noreply.gitlab.com>
2017-10-31 16:54:33 -0500

    Create RunningIndicator.java

---
+++ jconsplit/src/main/java/com/argon/jconsplit/RunningIndicator.java
@@ -0,0 +1,45 @@
+package com.argon.jconsplit;
+/**
+ *
+ * @author Argonitious
+ */
+public class RunningIndicator extends Thread
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    private String textmsg = "Working";
+    public void setText(String inString)
+    {
+        textmsg = inString;
+    }
+    public void run(){
+        while (true){
+            System.out.print(textmsg+".  \r");
+            try {
+            Thread.sleep(500);
+            } catch (InterruptedException e){
+                continue;
+            } catch (Exception e){
+                continue;
+            }
+            System.out.print(textmsg+".. \r");
+            try {
+            Thread.sleep(500);
+            } catch (InterruptedException e){
+                continue;
+            } catch (Exception e){
+                continue;
+            }
+            System.out.print(textmsg+"...\r");
+            try {
+            Thread.sleep(500);
+            } catch (InterruptedException e){
+                continue;
+            } catch (Exception e){
+                continue;
+            }
+        }
+    }
+}
